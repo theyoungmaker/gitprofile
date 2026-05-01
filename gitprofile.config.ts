b09642278dba7f6571c7934e7dd89b5730814a5e
Jack <jackchenyujie.96@gmail.com>
2026-05-01 17:43:14 +0800

    Update portfolio

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -30,14 +30,66 @@ const CONFIG = {
       },
     },
     external: {
-      header: 'My Projects',
+      header: 'Co-Curricular Activities & Achievements',
       // To hide the `External Projects` section, keep it empty.
-      projects: [
+  projects: [
+        {
+          title: 'Academic & Leadership',
+          description: `
+              Edusave Awards
+              - Edusave Certificate of Academic Achievement 2024
+              - Edusave Certificate of Academic Achievement 2023
+              - CCC - CDC Education Merit Award 2023
+
+               Leadership Assignments
+              - Senior Executive Committee (ExCo) Prefect 2026 (Primary 6)
+              - ExCo Prefect 2025 (Primary 5) 
+              - Produced video to explain roles and responsiblities of prefects, showcased during the Prefect Investiture Ceremony 2026
+              - Oragnised Teacher's Day Concert 2025 - Planning and execution of event, including coordinating with teachers, students, and vendors to ensure a successful celebration
+              - Mentored new ExCo Prefects in their duties and responsibilities, fostering leadership skills and teamwork
+              - Mentored incoming Primary 4 prefects in their duties and responsibilities, fostering leadership skills and teamwork
+
+            `,
+          imageUrl: 'https://i.ibb.co/WWCg8c83/5d4f8688-f463-4a4a-8a0d-39128de4c055.png',
+          link: 'https://drive.google.com/drive/u/8/folders/1tHj9k8zFhtxBVSEPkRz3gtM8GvWY5TXJ',
+        },
+        {
+          title: 'Science and Mathematics Achievements',
+          description: `- SJI 18th Matheylmpics (Singapore Primary Schools) (2025)
+                        - Certificate of Participation Singapore Primary Science Olympiad (2025)
+                        - Certificate of Award for Class 2 for Mental Arithmetic test in 82nd International Abacus & Mental Arithmetic Competition and Mathematics UnionTest (2025)
+                        - Certificate of Award for Class 5 for Abacus test in 82nd International Abacus & Mental Arithmetic Competition and Mathematics UnionTest (2025)
+                        - Certificate of Award for Class 6 for Abacus test in 80th International Abacus & Mental Arithmetic Competition and Mathematics UnionTest (2024)
+                        - Certificate of Award for Class 3 for Mental Arithmetic in 80th International Abacus & Mental Arithmetic Competition and Mathematics UnionTest (2024)
+                        - Certificate of Award for Class 4 for Mental Arithmetic in 78th International Abacus & Mental Arithmetic Competition and Mathematics UnionTest (2023)
+                        - Certificate of Award 76th International Abacus & Mental Arithmetic Competition and Mathematics UnionTest (2022)
+                       `,
+          imageUrl:
+            'https://i.ibb.co/XrMQF0Rq/5a478229-e3c6-421d-afe4-c381be1063ba.png',
+            link: 'https://drive.google.com/drive/u/8/folders/1tHj9k8zFhtxBVSEPkRz3gtM8GvWY5TXJ',
+        },
+        {
+          title: ' Coding and Robotics',
+          description: `First Lego League (2026) 
+                         - Represented The Young Maker in First Lego League Competition
+
+                        National Robotics Challenge (NRC) (2025)
+                        - Represented The Young Maker in National Robotics Challenge Open Category
+                        
+                       Coding Olympics Singapore Science Centre(2025)
+                        - Represented The Young Maker in Coding Olympics 2025
+
+                       Coding Olympics Singapore Science Centre (2024)
+                        - Represented The Young Maker in Coding Olympics 2024`,
+          imageUrl:
+            'https://i.ibb.co/RTZG7tg9/1bc6dcb8-a577-46db-a6cd-2fae42bb4b78.png',
+          link: 'https://drive.google.com/drive/u/8/folders/1-VsbvzqDINyM6KJN2FdS_ILzET_F5t9c',
+        },
       ],
     },
   },
   seo: {
-    title: 'Portfolio of The Young Maker',
+    title: 'Portfolio of The Isaac Poon Zheng Hao',
     description: '',
     imageURL: '',
   },
@@ -65,7 +117,7 @@ const CONFIG = {
     phone: '',
   },
   resume: {
-    fileUrl: '', // Empty fileUrl will hide the `Download Resume` button.
+    fileUrl: 'https://drive.google.com/drive/u/8/folders/1Jkssn6lSAiKId5itLYN8WuV3NCbWM6BN', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Scratch',
@@ -78,43 +130,54 @@ const CONFIG = {
     'MakeCode',
     'ScratchJr',
     'Hopscotch',
-    'Robotics',
+    'Robotics - LEGO Spike Prime',
+    'Robotics - LEGO WeDo 2.0',
     'Typescript',
+    'HTML, CSS '
+
   ],
   experiences: [
     {
-      company: 'None, i am still a kid',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
     },
   ],
   certifications: [
-    {
-      name: 'Python Beginner',
+      {
+      name: 'National Robotics Competition Open Category Primary',
+      body: 'National Robotics Competition by Science Center Singapore',
+      year: '2025',
+      link: 'https://drive.google.com/file/d/1WBKSH4Z0TllWd2DpJJSk3dU33VYl_9qq/view?usp=drive_link',
+    },
+      {
+      name: 'Coding Olympics 2025',
+      body: 'Science Centre Board Singapore',
+      year: '2025',
+      link: 'https://drive.google.com/file/d/1AHKrZnUsTPJDnbYd4tDiFNAGXd45ICtC/view?usp=drive_link',
+    },
+      {
+      name: 'Practical Python',
       body: 'The Young Maker',
-      year: '2024',
-      link: 'https://example.com',
+      year: '2025',
+      link: 'https://drive.google.com/file/d/1ro6DinZewyqpLFFX20WIV2x-AEBSFPMJ/view?usp=drive_link',
     },
     {
-      name: 'Python Advance',
+      name: 'Coding Olympics 2024',
+      body: 'Science Centre Board Singapore',
+      year: '2024',
+      link: 'https://drive.google.com/file/d/1Te40mAvlfp23kHz0RE42_kRCbaAe3Ie5/view?usp=drive_link',
+    },
+    {
+      name: 'Python Advanced',
       body: 'The Young Maker',
       year: '2024',
-      link: 'https://example.com',
+      link: 'https://drive.google.com/file/d/14XiDjy3ZdhQGQgcGlt-9q3BzNJtJnCxi/view?usp=drive_link',
     },
     {
       name: 'Python Intermediate',
       body: 'The Young Maker',
       year: '2024',
-      link: 'https://example.com',
+      link: 'https://drive.google.com/file/d/1xPj4yYGNAheyjwuahD8m6wxLr7WiKHuI/view?usp=drive_link',
     },
-    {
-      name: 'Practical Python',
-      body: 'The Young Maker',
-      year: '2025',
-      link: 'https://example.com',
-    },
+  
   ],
   educations: [
     {
@@ -140,11 +203,11 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'sunset',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
--- src/components/avatar-card/index.tsx
+++ src/components/avatar-card/index.tsx
@@ -89,7 +89,7 @@ const AvatarCard: React.FC<AvatarCardProps> = ({
               download
               rel="noreferrer"
             >
-              Download Resume
+              Take a look at my Achievements
             </a>
           ))}
       </div>
